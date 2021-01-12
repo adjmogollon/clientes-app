c2f83502afdcf043dc679034ab4e998d14be2bd8
Anibal Mogollon <adjmogollon@gmail.com>
2021-01-12 15:35:57 -0400

    S0212. Directiva estructural ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { Footercomponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, Footercomponent],
+  declarations: [AppComponent, HeaderComponent, Footercomponent, DirectivaComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+  listaCurso: String[] = ['TypeScript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+
+  constructor() {}
+}
