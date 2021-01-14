2d5daef6cdf1e0e9133efba4fb09dbb58ee7b07f
Anibal Mogollon <adjmogollon@gmail.com>
2021-01-14 19:05:28 -0400

    S0306. Implementando Observable en nuestra clase Servicio ClienteService

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 
-
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -13,6 +12,8 @@ export class ClientesComponent implements OnInit {
   constructor(private clienteService: ClienteService) {}
 
   ngOnInit(): void {
-    this.clientes = this.clienteService.getClientes();
+    this.clienteService
+      .getClientes()
+      .subscribe((clientes) => (this.clientes = clientes));
   }
 }
