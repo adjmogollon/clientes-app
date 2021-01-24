d7411412ea22ff59f94dc561b03e1a597f09fd7b
Anibal Mogollon <adjmogollon@gmail.com>
2021-01-23 20:34:54 -0400

    S1112. Afinando más el evento ngOnChanges para refrescar los rangos en el paginador

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -19,9 +19,18 @@ export class PaginatorComponent implements OnInit, OnChanges {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initPaginator();
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
+    let paginadorActualizado = changes['paginador'];
+    if (paginadorActualizado.previousValue) {
+      this.initPaginator();
+    }
+  }
+
+  private initPaginator(): void {
     this.desde = Math.min(
       Math.max(1, this.paginador.number - 4),
       this.paginador.totalPages - 5
