24dbea7277dcffd41a5d3f9a4b0904ddfb891168
Anibal Mogollon <adjmogollon@gmail.com>
2021-02-02 10:45:58 -0400

    S1310. Añadiendo Barra de Progreso

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpEvent,
+  HttpHeaders,
+  HttpRequest,
+} from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
@@ -103,17 +108,20 @@ export class ClienteService {
       );
   }
 
-  subirFoto(archivo: File, id): Observable<Cliente> {
+  subirFoto(archivo: File, id): Observable<HttpEvent<{}>> {
     let formData = new FormData();
     formData.append('archivo', archivo);
     formData.append('id', id);
-    return this.http.post(`${this.urlEndPoint}/upload`, formData).pipe(
-      map((response: any) => response.cliente as Cliente),
-      catchError((e) => {
-        console.error(e.error.mensaje);
-        Swal.fire(e.error.mensaje, e.error.error, 'error');
-        return throwError(e);
-      })
+
+    const req = new HttpRequest(
+      'POST',
+      `${this.urlEndPoint}/upload`,
+      formData,
+      {
+        reportProgress: true,
+      }
     );
+
+    return this.http.request(req);
   }
 }
--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -3,6 +3,7 @@ import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'detalle-cliente',
@@ -13,6 +14,7 @@ export class DetalleComponent implements OnInit {
   cliente: Cliente;
   titulo: string = 'Detalle del cliente';
   fotoSeleccionada: File;
+  progreso: number = 0;
   constructor(
     private clienteService: ClienteService,
     private activatedRoute: ActivatedRoute
@@ -29,6 +31,7 @@ export class DetalleComponent implements OnInit {
 
   seleccionarFoto(event): void {
     this.fotoSeleccionada = event.target.files[0];
+    this.progreso = 0;
     console.log(this.fotoSeleccionada);
     if (this.fotoSeleccionada.type.indexOf('image') < 0) {
       Swal.fire(
@@ -46,13 +49,18 @@ export class DetalleComponent implements OnInit {
     } else {
       this.clienteService
         .subirFoto(this.fotoSeleccionada, this.cliente.id)
-        .subscribe((cliente) => {
-          this.cliente = cliente;
-          Swal.fire(
-            'La foto se ha subido completamente',
-            `La foto se ha subido con exito: ${this.cliente.foto}`,
-            'success'
-          );
+        .subscribe((event) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progreso = Math.round((event.loaded / event.total) * 100);
+          } else if (event.type === HttpEventType.Response) {
+            let response: any = event.body;
+            this.cliente = response.cliente as Cliente;
+            Swal.fire(
+              'La foto se ha subido completamente',
+              response.mensaje,
+              'success'
+            );
+          }
         });
     }
   }
