cb100d31cb834c7e729f5c5caae2262f168815a8
Anibal Mogollon <adjmogollon@gmail.com>
2021-02-02 17:45:49 -0400

    S1315. Implementando EventEmitter para actualizar la foto en el listado

--- src/app/clientes/detalle/modal.service.ts
+++ src/app/clientes/detalle/modal.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root',
@@ -6,7 +7,13 @@ import { Injectable } from '@angular/core';
 export class ModalService {
   modal: boolean = false;
 
+  private _notificarUpload = new EventEmitter<any>();
+
   constructor() {}
+
+  get notificarUpload():EventEmitter<any>{
+    return this._notificarUpload;
+  }
 
   abrirModal(): void {
     this.modal = true;
