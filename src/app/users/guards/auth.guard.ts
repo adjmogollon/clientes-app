3107a3b885cae9824288634a6bf65ef9d84e86b5
Anibal Mogollon <adjmogollon@gmail.com>
2021-02-10 18:14:54 -0400

    S1617. Validando fecha de expiración del token en el AuthGuard

--- src/app/users/guards/auth.guard.ts
+++ src/app/users/guards/auth.guard.ts
@@ -15,11 +15,26 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
       if (this.authService.isAuthenticated()){
+        if (this.isTokenExpired()){
+          this.authService.loguot();
+          this.router.navigate(['/login']);
+          return true;
+        }
         return true;
       }
       this.router.navigate(['/login']);
       return false;
 
+  }
+
+  isTokenExpired(): boolean{
+    let token = this.authService.token;
+    let payload = this.authService.getPayload(token);
+    let now = new Date().getTime() /1000;
+    if (payload.exp < now){
+      return true;
+    }
+    return false;
   }
 
 }
